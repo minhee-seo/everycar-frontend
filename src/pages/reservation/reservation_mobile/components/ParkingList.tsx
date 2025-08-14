8c35b9b4ccfb2f605a7ac1a222c661e2e903f7a9
seo minhee <minheee07@gmail.com>
2025-08-14 16:46:35 +0900

    feature/map

--- src/pages/reservation/reservation_mobile/components/ParkingList.tsx
+++ src/pages/reservation/reservation_mobile/components/ParkingList.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import styles from './ParkingList.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLocationDot, faCar } from '@fortawesome/free-solid-svg-icons';
+import { faLocationDot, faCar, faFaceFrown } from '@fortawesome/free-solid-svg-icons';
 
 function ParkingList({ filtered }: { filtered: any[] }) {
     const [isExpanded, setIsExpanded] = useState(true);
@@ -11,30 +11,38 @@ function ParkingList({ filtered }: { filtered: any[] }) {
             <div className={styles.bar} onClick={() => setIsExpanded(!isExpanded)}>
                 <span className={styles.barIcon}></span>
             </div>
-            <ul className={!isExpanded ? styles.collapsed : ''}>
-                {filtered.map((parking, index) => {
-                    return (
-                        <li key={index}>
-                            <p className={styles.parkingName}>{parking.parking_name}</p>
-                            <div className={styles.information}>
-                                <div className={styles.info}>
-                                    <FontAwesomeIcon icon={faLocationDot} />
-                                    <p className={styles.parkingKm}>현재 위치에서 300m</p>
-                                </div>
-                                <div className={styles.info}>
-                                    <FontAwesomeIcon icon={faCar} />
-                                    <p className={styles.parkingNum}>3대 이용 가능</p>
-                                </div>
-                            </div>
-                            <div className={styles.addr}>
-                                <span>주소</span>
-                                <p className={styles.parkingAddr}>서울특별시 강남구 어쩌구 저쩌구</p>
-                            </div>
-                        </li>
-                    );
 
-                })}
-            </ul>
+            {
+                filtered.length === 0 ?
+                    <div className={styles.empty}>
+                        <FontAwesomeIcon icon={faFaceFrown} />
+                        <p>검색 결과가 없습니다.</p>
+                    </div> :
+                    <ul className={!isExpanded ? styles.collapsed : ''}>
+                        {filtered.map((parking, index) => {
+                            return (
+                                <li key={index}>
+                                    <p className={styles.parkingName}>{parking.parking_name}</p>
+                                    <div className={styles.information}>
+                                        <div className={styles.info}>
+                                            <FontAwesomeIcon icon={faLocationDot} />
+                                            <p className={styles.parkingKm}>현재 위치에서 300m</p>
+                                        </div>
+                                        <div className={styles.info}>
+                                            <FontAwesomeIcon icon={faCar} />
+                                            <p className={styles.parkingNum}>3대 이용 가능</p>
+                                        </div>
+                                    </div>
+                                    <div className={styles.addr}>
+                                        <span>주소</span>
+                                        <p className={styles.parkingAddr}>서울특별시 강남구 어쩌구 저쩌구</p>
+                                    </div>
+                                </li>
+                            );
+                        })}
+                    </ul>
+
+            }
         </div>
     )
 }
