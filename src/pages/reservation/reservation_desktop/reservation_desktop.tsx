19d978336f8628d2a187699bb18b74a415f1612a
seominhee <minheee07@gmail.com>
2025-08-26 18:24:17 +0900

    검색창 디자인 수정

--- src/pages/reservation/reservation_desktop/reservation_desktop.tsx
+++ src/pages/reservation/reservation_desktop/reservation_desktop.tsx
@@ -54,25 +54,31 @@ function Reservation() {
                   <>
                     <h3>{keyword} 에브리카 대여소</h3>
                     <ul className={styles.collapsed}>
-                      <li>
-                        <p className={styles.parkingName}>강남역 주차장</p>
-                        <div className={styles.information}>
-                          <div className={styles.info}>
-                            <FontAwesomeIcon icon={faLocationDot} />
-                            <p className={styles.parkingKm}>현재 위치에서 300m</p>
-                          </div>
-                          <div className={styles.info}>
-                            <FontAwesomeIcon icon={faCar} />
-                            <p className={styles.parkingNum}>3대 이용 가능</p>
-                          </div>
-                        </div>
-                        <div className={styles.addr}>
-                          <p className={styles.parkingAddr}>서울특별시 강남구 어쩌구 저쩌구</p>
-                        </div>
-                      </li>
+                      {
+                        filtered.map((parking, index) => {
+                          return (
+                            <li key={`parking${index}`}>
+                              <p className={styles.parkingName}>{parking.parking_name}</p>
+                              <div className={styles.information}>
+                                <div className={styles.info}>
+                                  <FontAwesomeIcon icon={faLocationDot} />
+                                  <p className={styles.parkingKm}>현재 위치에서 300m</p>
+                                </div>
+                                <div className={styles.info}>
+                                  <FontAwesomeIcon icon={faCar} />
+                                  <p className={styles.parkingNum}>3대 이용 가능</p>
+                                </div>
+                              </div>
+                              <div className={styles.addr}>
+                                <p className={styles.parkingAddr}>서울특별시 강남구 어쩌구 저쩌구</p>
+                              </div>
+                            </li>
+                          );
+                        })
+                      }
                     </ul>
                   </>
-                }
+              }
             </div>
 
             <div className={styles.selectArea}>
