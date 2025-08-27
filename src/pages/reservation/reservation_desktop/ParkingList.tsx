e6d92916ce68d4d0b99756ba4c94edb35db5ac2d
seo minhee <minheee07@gmail.com>
2025-08-27 18:58:02 +0900

    이용 가능 차량 버튼 생성

--- src/pages/reservation/reservation_desktop/ParkingList.tsx
+++ src/pages/reservation/reservation_desktop/ParkingList.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 import styles from './ParkingList.module.scss';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFaceFrown, faClock, faAngleRight, faLocationDot, faCar, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { faLocationCrosshairs, faLocationDot, faCar, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
-interface ParkingListProps{
+interface ParkingListProps {
     parking: {
         parking_id: number,
         parking_name: string,
@@ -16,36 +16,36 @@ interface ParkingListProps{
 }
 
 const ParkingList: React.FC<ParkingListProps> = ({ parking, map }) => {
-  const handleClick = () => {
-    if (!map) return;
+    const handleClick = () => {
+        if (!map) return;
 
-    const position = new window.kakao.maps.LatLng(
-      parking.parking_latitude,
-      parking.parking_longtitude
+        const position = new window.kakao.maps.LatLng(
+            parking.parking_latitude,
+            parking.parking_longtitude
+        );
+
+        map.panTo(position); // 부드럽게 이동
+        map.setLevel(3); // 필요 시 확대
+    };
+
+    return (
+        <li className={styles.parkingItem} onClick={handleClick}>
+            <p className={styles.parkingName}>{parking.parking_name}</p>
+            <div className={styles.information}>
+                <div className={styles.info}>
+                    <FontAwesomeIcon icon={faLocationCrosshairs} />
+                    <p className={styles.parkingKm}>현재 위치에서 300m</p>
+                    <FontAwesomeIcon icon={faCar} />
+                    <p className={styles.parkingNum}>3대 이용 가능</p>
+                    <FontAwesomeIcon icon={faLocationDot} />
+                    <p className={styles.parkingAddr}>{parking.parking_address}</p>
+                </div>
+            </div>
+            <button className={styles.viewCarsButton} >
+                이용 가능 차량 보기
+            </button>
+        </li >
     );
-
-    map.panTo(position); // 부드럽게 이동
-    map.setLevel(3); // 필요 시 확대
-  };
-
-  return (
-    <li className={styles.parkingItem} onClick={handleClick}>
-      <p className={styles.parkingName}>{parking.parking_name}</p>
-      <div className={styles.information}>
-        <div className={styles.info}>
-          <FontAwesomeIcon icon={faLocationDot} />
-          <p className={styles.parkingKm}>현재 위치에서 300m</p>
-        </div>
-        <div className={styles.info}>
-          <FontAwesomeIcon icon={faCar} />
-          <p className={styles.parkingNum}>3대 이용 가능</p>
-        </div>
-      </div>
-      <div className={styles.addr}>
-        <p className={styles.parkingAddr}>{parking.parking_address}</p>
-      </div>
-    </li>
-  );
 };
 
 export default ParkingList
