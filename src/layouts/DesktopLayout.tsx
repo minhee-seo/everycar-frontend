1b06e0370e9ff106b9103551e844e4b9d9636b28
seominhee <minheee07@gmail.com>
2025-09-16 16:11:58 +0900

    pc화면 특정 페이지에서 footer을 없애기

--- src/layouts/DesktopLayout.tsx
+++ src/layouts/DesktopLayout.tsx
@@ -1,17 +1,25 @@
 import ResponsiveSwitch from '../components/responsive/ResponsiveSwitch.tsx';
 import Header from '../components/common/header/header_desktop/DesktopGnb.tsx';
 import Footer from '../components/common/Footer.tsx';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 
-const DesktopLayout = () => (
-  <>
+const DesktopLayout = () => {
+  const location = useLocation();
+
+  // Footer를 숨길 경로 목록
+  const hideFooterPaths = ["/login", "/reservation"];
+
+  const shouldHideFooter = hideFooterPaths.includes(location.pathname);
+
+  return (
     <div className="layout-wrapper">
       <Header />
       <div className="content">
         <Outlet />
       </div>
+      {!shouldHideFooter && <Footer />}
     </div>
-  </>
-);
+  );
+};
 
 export default DesktopLayout;
