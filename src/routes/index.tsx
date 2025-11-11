ebb0ce6db5206a68f53bcca06258c0c72f745773
seominhee <minheee07@gmail.com>
2025-11-11 20:19:38 +0900

    로그인 페이지

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -12,6 +12,7 @@ import '../layouts/layout.scss';
 import CarList from '../pages/carList/CarList.tsx';
 import CarDetail from '../pages/carDetail/CarDetail.tsx';
 import Payment from '../pages/payment/Payment.tsx';
+import Login from '../pages/Auth/Login.tsx';
 
 const AppRoutes = () => (
   <>
@@ -23,6 +24,7 @@ const AppRoutes = () => (
           <Route path="/reservation/carList" element={<CarList />} />
           <Route path="/reservation/carDetail" element={<CarDetail />} />
           <Route path="/reservation/payment" element={<Payment />} />
+          <Route path="/auth/login" element={<Login />} />
         </Route>
       </Routes>
       
